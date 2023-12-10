cac0e66c7b81c2e2a4e1395735460dcea71de6aa
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-11 05:31:32 +0700

    feat(chat-realtime): update controller create roomchat

--- controllers/chat-realtime.go
+++ controllers/chat-realtime.go
@@ -1,10 +1,59 @@
 package controllers
 
-import "github.com/labstack/echo/v4"
+import (
+	"healthcare/configs"
+	"healthcare/models/schema"
+	"healthcare/utils/helper"
+	"healthcare/utils/request"
+	"healthcare/utils/response"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 // User Create Roomchat and Send Notification to Doctor
 func CreateRoomchatsController(c echo.Context) error {
-	return nil
+
+	userID, ok := c.Get("userID").(int)
+	if !ok {
+		return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("invalid user id"))
+	}
+
+	transactionID, err := strconv.Atoi(c.Param("transaction_id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, helper.ErrorResponse("invalid transaction id"))
+	}
+
+	var existingRoomchat schema.Roomchat
+	if err := configs.DB.First(&existingRoomchat, "transaction_id = ?", transactionID).Error; err == nil {
+		return c.JSON(http.StatusConflict, helper.ErrorResponse("roomchat for this transaction id already exists"))
+	}
+
+	var doctorTransaction schema.DoctorTransaction
+	if err := configs.DB.First(&doctorTransaction, "user_id = ? AND id = ? AND payment_status = 'success'", userID, transactionID).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("failed to retrieve doctor transaction data"))
+	}
+
+	roomchat := request.CreateRoomchatRequest(uint(transactionID))
+
+	if err := configs.DB.Create(&roomchat).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.ErrorResponse("failed to create roomchat"))
+	}
+
+	response := response.ConvertToCreateRoomchatResponse(&roomchat)
+
+	return c.JSON(http.StatusCreated, helper.SuccessResponse("roomchat created successful", response))
+
 }
 
 // User or Doctor Join Roomchat
