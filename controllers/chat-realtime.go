74f1c2893cad91da810bad371ff0fabd43e9cfe0
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-11 05:17:07 +0700

    feat(chat-realtime): add controllers chat realtime

---
+++ controllers/chat-realtime.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "github.com/labstack/echo/v4"
+
+// User Create Roomchat and Send Notification to Doctor
+func CreateRoomchatsController(c echo.Context) error {
+	return nil
+}
+
+// User or Doctor Join Roomchat
+func JoinRoomchatController(c echo.Context) error {
+	return nil
+}
+
+// User or Doctor Get Roomchat
+func GetRoomchatController(c echo.Context) error {
+	return nil
+}
+
+// Doctor Get All Roomchats
+func GetAllRoomchatsController(c echo.Context) error {
+	return nil
+}
+
+// Doctor Get Client
+func GetClientController(c echo.Context) error {
+	return nil
+}
